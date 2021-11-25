9914b2894861d2b8a4497dd8d268b0645a864c5c
Shujja <djshujja@gmail.com>
2021-11-26 03:26:56 +0500

    owl carousel --clients

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -5,19 +5,20 @@ $(".owl-carousel").owlCarousel({
   loop: true,
   margin: 10,
   responsiveClass: true,
+  center: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  autoplayHoverPause: true,
+  fluidSpeed: true,
   responsive: {
     0: {
-      items: 1,
-      nav: true
+      items: 1
     },
     600: {
-      items: 3,
-      nav: false
+      items: 3
     },
     1000: {
-      items: 5,
-      nav: true,
-      loop: false
+      items: 5
     }
   }
 });
--- script.js
+++ script.js
@@ -3,19 +3,20 @@ $(".owl-carousel").owlCarousel({
   loop: true,
   margin: 10,
   responsiveClass: true,
+  center: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  autoplayHoverPause: true,
+  fluidSpeed: true,
   responsive: {
     0: {
       items: 1,
-      nav: true,
     },
     600: {
       items: 3,
-      nav: false,
     },
     1000: {
       items: 5,
-      nav: true,
-      loop: false,
     },
   },
 });
