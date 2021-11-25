966aff43a1abdf94f319401e3e04c1c3bb60f073
Shujja <djshujja@gmail.com>
2021-11-26 03:16:02 +0500

    our promises

---
+++ dist/script.dev.js
@@ -0,0 +1,23 @@
+"use strict";
+
+console.log("I'm connected! c:");
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true
+    },
+    600: {
+      items: 3,
+      nav: false
+    },
+    1000: {
+      items: 5,
+      nav: true,
+      loop: false
+    }
+  }
+});
