b497256e0f67ee98b951f767d31fe301630941b7
Shujja <djshujja@gmail.com>
2021-12-30 17:35:04 +0500

    formspree

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 console.log("I'm connected! c:");
 
+// Form Ajax
+
+// var form = document.getElementById("contact-form");
+
+// async function handleSubmit(event) {
+//   event.preventDefault();
+//   var status = document.getElementById("my-form-status");
+
+//   var data = new FormData(event.target);
+//   fetch(event.target.action, {
+//     method: form.method,
+//     body: data,
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   })
+//     .then((response) => {
+//       status.innerHTML = "Thanks for your submission!";
+//       form.reset();
+//     })
+//     .catch((error) => {
+//       status.innerHTML = "Oops! There was a problem submitting your form";
+//     });
+// }
+// form.addEventListener("submit", handleSubmit);
+
 var $window = $(window),
   $main = $("div.main");
 
